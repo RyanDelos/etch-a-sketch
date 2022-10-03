7f31396effaef46e7d58d69a158724dee148ad8f
Ryan Delos Santos <delossantosrryan@gmail.com>
2022-10-02 22:40:18 -0700

    Added JS functions for grid

--- script.js
+++ script.js
@@ -1,5 +1,44 @@
 'use strict';
 
-const grid = document.querySelector('.grid');
+// Variable Declaration
+const DEFAULT_SIZE = 16;
+const button = document.querySelector('.btn');
+const grid = document.querySelector('.grid-container');
 
-console.log(grid);
+// makeRows function creates 16x16 grid
+function makeGrid(size) {
+  grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  for (let c = 0; c < size * size; c++) {
+    let cell = document.createElement('div');
+    // cell.innerText = c + 1;
+    grid.appendChild(cell).className = 'grid-item';
+  }
+}
+makeGrid(DEFAULT_SIZE);
+
+function clearGrid() {
+  grid.innerHTML = '';
+}
+// hover effect
+function hover() {
+  let gridCell = document.querySelectorAll('.grid-item');
+  for (let i = 0; i < gridCell.length; i++) {
+    gridCell[i].addEventListener('mouseover', (e) => {
+      gridCell[i].style.backgroundColor = 'black';
+    });
+  }
+}
+hover();
+
+button.addEventListener('click', function () {
+  let gridValue = prompt('How many cells?');
+  clearGrid();
+  if (gridValue >= 1 && gridValue <= 64) {
+    makeGrid(gridValue, gridValue);
+    hover();
+  } else {
+    alert(`Number has to be between 1 through 64`);
+  }
+});
